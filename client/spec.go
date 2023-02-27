1eaca387fbadd04fe922e26f0a696aff46105009
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2023-02-26 20:03:05 -0600

    Auth spec validation

--- client/spec.go
+++ client/spec.go
@@ -10,16 +10,19 @@ import (
 // Spec is the configuration for a SharePoint source
 type Spec struct {
 	// Gosip auth config connection params https://go.spflow.com/auth/overview
-	Auth struct {
-		// Auth strategy: azurecert, azurecreds, device, saml, addin, adfs, ntlm, tmg, fba
-		Strategy string `json:"strategy"`
-		// `creds` options are unique for different auth strategies. See more details in [Auth strategies](https://go.spflow.com/auth/strategies)
-		Creds map[string]string `json:"creds"`
-	} `json:"auth"`
+	Auth AuthSpec `json:"auth"`
 
 	// A map of URIs to the list configuration
 	// If no lists are provided, nothing will be fetched
 	Lists map[string]ListSpec `json:"lists"`
+}
+
+// AuthSpec is the configuration for a SharePoint auth
+type AuthSpec struct {
+	// Auth strategy: azurecert, azurecreds, device, saml, addin, adfs, ntlm, tmg, fba
+	Strategy string `json:"strategy"`
+	// `creds` options are unique for different auth strategies. See more details in [Auth strategies](https://go.spflow.com/auth/strategies)
+	Creds map[string]string `json:"creds"`
 }
 
 // ListSpec is the configuration for a list source
@@ -60,6 +63,29 @@ func (s *Spec) SetDefaults() {
 	}
 }
 
+// Validate validates SharePoint source spec validity
+func (s *Spec) Validate() error {
+	// Validation auth options
+	if err := s.Auth.Validate(); err != nil {
+		return err
+	}
+
+	// All lists should have unique aliases
+	aliases := make(map[string]bool)
+	for listURI, listSpec := range s.Lists {
+		alias := strings.ToLower(listSpec.Alias)
+		if alias == "" {
+			alias = strings.ToLower(listURI)
+		}
+		if _, ok := aliases[alias]; ok {
+			return fmt.Errorf("duplicate alias \"%s\" for list \"%s\" configuration", alias, listURI)
+		}
+		aliases[alias] = true
+	}
+
+	return nil
+}
+
 // SetDefault sets default values for list spec
 func (l *ListSpec) SetDefault() {
 	if l.Select == nil {
@@ -77,30 +103,50 @@ func (l *ListSpec) SetDefault() {
 	l.Select = concatSlice(prepProps, concatSlice(l.Select, apndProps))
 }
 
-// Validate validates SharePoint source spec validity
-func (s Spec) Validate() error {
-	if s.Auth.Strategy == "" {
-		return fmt.Errorf("auth.strategy is required")
-	}
-	if len(s.Auth.Creds) == 0 {
-		return fmt.Errorf("auth.creds is required")
+// Validate validates auth spec validity
+func (a *AuthSpec) Validate() error {
+	docs := "https://go.spflow.com/auth/strategies"
+	strategies := map[string]struct {
+		fields []string
+		docs   string
+	}{
+		"azurecert":  {fields: []string{"siteUrl", "tenantId", "clientId", "certPath", "certPass"}, docs: "https://go.spflow.com/auth/strategies/azure-certificate-auth"},
+		"azurecreds": {fields: []string{"siteUrl", "tenantId", "clientId", "username", "password"}, docs: "https://go.spflow.com/auth/strategies/azure-creds-auth"},
+		"addin":      {fields: []string{"siteUrl", "clientId", "clientSecret"}, docs: "https://go.spflow.com/auth/strategies/addin"},
+		"device":     {fields: []string{"siteUrl", "tenantId", "clientId"}, docs: "https://go.spflow.com/auth/strategies/azure-device-flow"},
+		"saml":       {fields: []string{"siteUrl", "username", "password"}, docs: "https://go.spflow.com/auth/strategies/saml"},
+		"ntlm":       {fields: []string{"siteUrl", "username", "password"}, docs: "https://go.spflow.com/auth/strategies/ntlm"},
+		"adfs":       {fields: []string{"siteUrl", "username", "password"}, docs: "https://go.spflow.com/auth/strategies/adfs"},
+		"fba":        {fields: []string{"siteUrl", "username", "password"}, docs: "https://go.spflow.com/auth/strategies/fba"},
+		"tmg":        {fields: []string{"siteUrl", "username", "password"}, docs: "https://go.spflow.com/auth/strategies/tmg"},
 	}
 
-	if len(s.Lists) == 0 {
-		return fmt.Errorf("no lists configuration is provided")
+	strategiesList := funk.Keys(strategies).([]string)
+
+	// Missing strategy
+	if a.Strategy == "" {
+		return fmt.Errorf("missing auth strategy, use one of these: %s; see more %s", strings.Join(strategiesList, ", "), docs)
 	}
 
-	// All lists should have unique aliases
-	aliases := make(map[string]bool)
-	for listURI, listSpec := range s.Lists {
-		alias := strings.ToLower(listSpec.Alias)
-		if alias == "" {
-			alias = strings.ToLower(listURI)
+	// Check if strategy is supported
+	if _, ok := strategies[a.Strategy]; !ok {
+		return fmt.Errorf("unsupported auth strategy \"%s\", use one of these: %s; see more %s", a.Strategy, strings.Join(strategiesList, ", "), docs)
+	}
+
+	// Check if creds are provided
+	if a.Creds == nil {
+		return fmt.Errorf("missing auth creds for \"%s\" auth strategy, required props: %s; see more %s", a.Strategy, strings.Join(strategies[a.Strategy].fields, ", "), strategies[a.Strategy].docs)
+	}
+
+	// Check if all required fields are provided
+	missedFields := []string{}
+	for _, field := range strategies[a.Strategy].fields {
+		if _, ok := a.Creds[field]; !ok {
+			missedFields = append(missedFields, field)
 		}
-		if _, ok := aliases[alias]; ok {
-			return fmt.Errorf("duplicate alias \"%s\" for list \"%s\" configuration", alias, listURI)
-		}
-		aliases[alias] = true
+	}
+	if len(missedFields) > 0 {
+		return fmt.Errorf("missing required field(s) \"%s\" for \"%s\" auth strategy; see more %s", strings.Join(missedFields, ", "), a.Strategy, strategies[a.Strategy].docs)
 	}
 
 	return nil
